610685273dd02db2e31b136bbd7f0e3667281e35
Hayato Anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2017-10-03 18:39:26 +0900

    add cython to dependencies

--- setup.py
+++ setup.py
@@ -2,24 +2,32 @@
 import sys
 import os
 import shutil
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
-import numpy
-import pysam
+IMPORTERROR_FMT = "Failed to import {}. Install build time dependencies " \
+                  "(e.g. `pip install cython numpy pysam`) and try again."
+try:
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext
+except ImportError:
+    sys.exit(IMPORTERROR_FMT.format("Cython"))
+try:
+    import numpy
+except ImportError:
+    sys.exit(IMPORTERROR_FMT.format("numpy"))
+try:
+    import pysam
+except ImportError:
+    sys.exit(IMPORTERROR_FMT.format("pysam"))
 
 BASEDIR = os.path.dirname(__file__)
+
 NUMPY_INCLUDE = numpy.get_include()
 PYSAM_PATH = pysam.__path__[0]
 PYSAM_INCLUDES = [PYSAM_PATH, os.path.join(PYSAM_PATH, "include", "htslib")]
-EXTRA_C_ARGS = ["-O3", "-ffast-math"]
+sys.path.append(os.path.join(BASEDIR, "external", "bx-python", "lib"))
 
-try:
-    CYTHON_BUILD = True
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-    sys.path.append(os.path.join(BASEDIR, "external", "bx-python", "lib"))
-except ImportError:
-    CYTHON_BUILD = False
+EXTRA_C_ARGS = ["-O3", "-ffast-math"]
 
 
 def _basedir(path):
@@ -27,55 +35,23 @@ def _basedir(path):
 
 
 def _link_source():
-    exts = (".pyx", ".pxd") if CYTHON_BUILD else (".c", )
-
-    for ext in exts:
+    for ext in (".pyx", ".pxd"):
         shutil.copy2(_basedir("PyMaSC/utils/bx/binary_{}_endian".format(sys.byteorder) + ext),
                      _basedir("PyMaSC/utils/bx/binary_file" + ext))
 
 
 def _define_extension(name, include_numpy=False):
-    ext = ".pyx" if CYTHON_BUILD else ".c"
     include_dirs = [NUMPY_INCLUDE] if include_numpy else []
 
     return Extension(
-        name, sources=[_basedir(name.replace('.', '/') + ext)],
+        name, sources=[_basedir(name.replace('.', '/') + ".pyx")],
         include_dirs=include_dirs, extra_compile_args=EXTRA_C_ARGS
     )
-
-
-def _get_setup_args():
-    cmdclass = {} if not CYTHON_BUILD else {"build_ext": build_ext}
-    ext_modules = [
-        _define_extension(name, include_numpy=True) for name in (
-            "PyMaSC.reader.bx.bbi_file",
-            "PyMaSC.reader.bx.bigwig_file",
-            "PyMaSC.reader.bigwig",
-            "PyMaSC.core.mappability",
-            "PyMaSC.core.ncc",
-            "PyMaSC.core.mscc"
-        )
-    ] + [
-        _define_extension(name) for name in (
-            "PyMaSC.reader.bx.bpt_file",
-            "PyMaSC.utils.bx.binary_file"
-        )
-    ] + [Extension(
-        "PyMaSC.core.readlen",
-        sources=[_basedir("PyMaSC/core/readlen" + (".pyx" if CYTHON_BUILD else ".c"))],
-        include_dirs=PYSAM_INCLUDES,
-        extra_compile_args=EXTRA_C_ARGS
-    )]
-
-    return cmdclass, ext_modules
 
 
 def _setup():
     #
     _link_source()
-    cmdclass, ext_modules = _get_setup_args()
-    if CYTHON_BUILD:
-        ext_modules = cythonize(ext_modules)
 
     #
     setup(
@@ -111,8 +87,30 @@ def _setup():
             "bx-python>=0.7.3",
             "matplotlib>=2.0.0"
         ],
-        cmdclass=cmdclass,
-        ext_modules=ext_modules,
+        packages=find_packages(),
+        cmdclass={"build_ext": build_ext},
+        ext_modules=cythonize([
+            _define_extension(name, include_numpy=True) for name in (
+                "PyMaSC.reader.bx.bbi_file",
+                "PyMaSC.reader.bx.bigwig_file",
+                "PyMaSC.reader.bigwig",
+                "PyMaSC.core.mappability",
+                "PyMaSC.core.ncc",
+                "PyMaSC.core.mscc"
+            )
+        ] + [
+            _define_extension(name) for name in (
+                "PyMaSC.reader.bx.bpt_file",
+                "PyMaSC.utils.bx.binary_file"
+            )
+        ] + [
+            Extension(
+                "PyMaSC.core.readlen",
+                sources=[_basedir("PyMaSC/core/readlen.pyx")],
+                include_dirs=PYSAM_INCLUDES,
+                extra_compile_args=EXTRA_C_ARGS
+            )
+        ]),
         entry_points={
             "console_scripts": [
                 "pymasc = PyMaSC.pymasc:exec_entrypoint",

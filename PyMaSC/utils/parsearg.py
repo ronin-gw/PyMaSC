7dc718e873fccd3c03ab7225c6e9c17d6b7e0759
Hayato Anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2017-12-05 00:22:50 +0900

    change default moving average window size 30 to 15

--- PyMaSC/utils/parsearg.py
+++ PyMaSC/utils/parsearg.py
@@ -73,8 +73,8 @@ def get_parser():
                         help="Filter out reads which have less than specified MAPQ. (Default: 1)")
     params.add_argument("--chi2-pval", nargs='?', type=float, default=0.05,
                         help="p-value threshold for Chi-squared test to check strand specificity. (Default: 0.05)")
-    params.add_argument("-w", "--smooth-window", nargs='?', type=int, action=ForceNaturalNumber, default=30,
-                        help="Moving average window size for smoothing MSCC to estimate library length. (Default: 30)")
+    params.add_argument("-w", "--smooth-window", nargs='?', type=int, action=ForceNaturalNumber, default=15,
+                        help="Moving average window size for smoothing MSCC to estimate library length. (Default: 15)")
     params.add_argument("--skip-ncc", action="store_true",
                         help="Skip naive cross-correlation calculation. Mappability region file must be specified.")
 

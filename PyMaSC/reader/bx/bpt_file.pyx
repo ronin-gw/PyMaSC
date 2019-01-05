aecad7f872a80b49fc28c005624eebb630b5592b
Hayato anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2019-01-06 05:59:01 +0900

    fix bpt processing

--- PyMaSC/reader/bx/bpt_file.pyx
+++ PyMaSC/reader/bx/bpt_file.pyx
@@ -4,6 +4,38 @@ from bx.bbi.types cimport UBYTE, bits16, bits64
 
 
 cdef class BPTFile(bxBPTFile):
+    def r_find( self, bits64 block_start, key ):
+        """
+        Recursively seek the value matching key under the subtree starting
+        at file offset `block_start`
+        """
+        cdef UBYTE is_leaf
+        cdef bits16 child_count
+        cdef bits64 offset
+        self.reader.seek( block_start )
+        # Block header
+        is_leaf = self.reader.read_uint8()
+        self.reader.read_uint8()
+        child_count = self.reader.read_uint16()
+        if is_leaf:
+            for i from 0 <= i < child_count:
+                node_key = self.reader.read( self.key_size )
+                node_value = self.reader.read( self.value_size )
+                if node_key == key:
+                    return node_value
+            return None
+        else:
+            # Read and discard first key, store offset
+            self.reader.read( self.key_size )
+            offset = self.reader.read_uint64()
+            # Loop until correct subtree is found
+            for i from 0 <= i < child_count - 1:
+                node_key = self.reader.read( self.key_size )
+                if node_key > key:
+                    break
+                offset = self.reader.read_uint64()
+            return self.r_find( offset, key )
+
     def find( self, key ):
         """
         Find the value matching `key` (a bytestring). Returns the matching
@@ -23,7 +55,7 @@ cdef class BPTFile(bxBPTFile):
         """
         cdef UBYTE is_leaf
         cdef bits16 child_count
-        cdef bits64 offset
+        cdef bits64 offset, backpoint
         self.reader.seek( block_start )
         # Block header
         is_leaf = self.reader.read_uint8()
@@ -35,14 +67,15 @@ cdef class BPTFile(bxBPTFile):
                 node_value = self.reader.read( self.value_size )
                 yield node_key, node_value
         else:
-            # Read and discard first key, store offset
-            self.reader.read( self.key_size )
-            offset = self.reader.read_uint64()
-            # Loop until correct subtree is found
+            # Traverse all subtree
             for i from 0 <= i < child_count:
                 self.reader.skip( self.key_size )
                 offset = self.reader.read_uint64()
-            yield self.traverse( offset )
+                if offset == 0:
+                    break
+                backpoint = self.reader.tell()
+                yield from self.r_traverse( offset )
+                self.reader.seek( backpoint )
 
     def traverse(self):
         """

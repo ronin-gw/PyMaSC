fc9ee67890f2f9e220839c7ae9b5935f6fc835b0
Hayato anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2019-01-05 14:39:37 +0900

    check compatibility

--- setup.py
+++ setup.py
@@ -125,8 +125,10 @@ def _setup():
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: Implementation :: CPython",
             "Topic :: Scientific/Engineering :: Bio-Informatics"
         ],

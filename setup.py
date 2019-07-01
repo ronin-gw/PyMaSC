0471fee771dc00e8fbafac76e5c74206a521014f
Hayato Anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2019-07-02 03:37:23 +0900

    fix scipy versions

--- setup.py
+++ setup.py
@@ -138,13 +138,12 @@ def _setup():
         install_requires=[
             "numpy>=1.12.0",
             "pysam==0.15.1",
-            "scipy>=0.18.1",
             "bx-python>=0.7.3"
         ],
         extras_require={
-            ':python_version<"3.5"': ["matplotlib>=2.0.0,<3.0.0"],
-            ':python_version<"3.6"': ["matplotlib>=2.0.0,<3.1.0"],
-            ':python_version>="3.6"': ["matplotlib>=2.0.0"]
+            ':python_version<"3.5"': ["matplotlib>=2.0.0,<3.0.0", "scipy>=1.2.0,<1.3.0"],
+            ':python_version<"3.6"': ["matplotlib>=2.0.0,<3.1.0", "scipy>=1.3.0"],
+            ':python_version>="3.6"': ["matplotlib>=2.0.0", "scipy>=1.3.0"]
         },
         packages=find_packages(),
         cmdclass={"build_ext": BuildExtCommand},

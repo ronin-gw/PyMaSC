f3dd7c59db0bf2d2fcb2b6bcd878ab539feb85ab
Hayato anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2019-07-01 20:39:15 +0900

    use Fisher transformation to merge cc

--- PyMaSC/handler/result.py
+++ PyMaSC/handler/result.py
@@ -3,6 +3,7 @@ from functools import wraps
 
 import numpy as np
 from scipy.stats.distributions import chi2
+from scipy.stats import norm
 
 from PyMaSC.handler.mappability import MappabilityHandler
 from PyMaSC.utils.calc import moving_avr_filter
@@ -10,6 +11,7 @@ from PyMaSC.utils.calc import moving_avr_filter
 logger = logging.getLogger(__name__)
 
 NEAR_READLEN_ERR_CRITERION = 5
+MERGED_CC_CONFIDENCE_INTERVAL = 0.95
 
 
 def _skip_none(i):
@@ -241,21 +243,80 @@ class ReadsTooFew(IndexError):
 
 
 class CCResult(object):
-    def __init__(
-        self, handler=None,
-        filter_len=15, chi2_pval=0.05, expected_library_len=None, filter_mask_len=NEAR_READLEN_ERR_CRITERION
-    ):
+    @classmethod
+    def _merge_cc(cls, ns, ccs):
+        zs = np.arctanh(ccs)
+        sum_n3 = sum(n - 3 for n in ns)
+        avr_z = np.sum(((n - 3) * z for n, z in zip(ns, zs)), axis=0) / sum_n3
+
+        z_interval = norm.ppf(1 - (1 - MERGED_CC_CONFIDENCE_INTERVAL) / 2) * np.sqrt(1 / sum_n3)
+        z_interval_upper = avr_z + z_interval
+        z_interval_lower = avr_z - z_interval
+
+        merged_r = np.tanh(avr_z)
+        interval_upper = np.tanh(z_interval_upper)
+        interval_lower = np.tanh(z_interval_lower)
+
+        return merged_r, interval_lower, interval_upper
+
+    def __init__(self, handler=None,
+                 read_len=None, lengths=None, references=None,
+                 ref2ccbins=None, ref2mappable_len=None, mappable_ref2ccbins=None,
+                 filter_len=15, chi2_pval=0.05, expected_library_len=None,
+                 filter_mask_len=NEAR_READLEN_ERR_CRITERION):
+
         # settings
         self.filter_len = filter_len
         self.chi2_p_thresh = chi2_pval
         self.expected_library_len = expected_library_len
         self.filter_mask_len = max(filter_mask_len, 0)
 
+        if handler:
+            self._init_from_handler(handler)
+        else:
+            self.read_len = read_len
+            self.lengths = lengths
+            self.references = references
+            self.ref2ccbins = ref2ccbins
+            self.ref2mappable_len = ref2mappable_len
+            self.mappable_ref2ccbins = mappable_ref2ccbins
+
+            self.skip_ncc = not (self.lengths and self.ref2ccbins)
+            self.calc_masc = self.ref2mappable_len and self.mappable_ref2ccbins
+
+            assert (not self.skip_ncc) or self.calc_masc
+
         #
-        self.references = references = handler.references
-        lengths = handler.lengths
-        ref2genomelen = dict(zip(references, lengths))
-        read_len = handler.read_len
+        if not self.skip_ncc:
+            ncc, self.ncc_upper, self.ncc_lower = self._merge_cc(
+                self.lengths,
+                [self.ref2ccbins[ref] for ref in self.references]
+            )
+        else:
+            ncc = self.ncc_upper = self.ncc_lower = None
+
+        if self.calc_masc:
+            mscc, self.mscc_upper, self.mscc_lower = self._merge_cc(
+                [self.ref2mappable_len[ref] for ref in self.references],
+                [self.mappable_ref2ccbins[ref] for ref in self.references]
+            )
+        else:
+            mscc = self.mscc_upper = self.mscc_lower = None
+
+        self.whole = PyMaSCStats(
+            self.read_len, self.filter_len, self.expected_library_len,
+            cc=ncc,
+            masc=mscc,
+            warning=True,
+            filter_mask_len=self.filter_mask_len
+        )
+
+    def _init_from_handler(self, handler):
+        #
+        self.references = handler.references
+        self.lengths = handler.lengths
+        ref2genomelen = dict(zip(self.references, self.lengths))
+        self.read_len = handler.read_len
         ref2forward_sum = handler.ref2forward_sum
         ref2reverse_sum = handler.ref2reverse_sum
         ref2ccbins = handler.ref2ccbins
@@ -267,7 +328,7 @@ class CCResult(object):
         #
         self.ref2stats = {
             ref: PyMaSCStats(
-                read_len, filter_len, expected_library_len,
+                self.read_len, self.filter_len, self.expected_library_len,
                 genomelen=ref2genomelen[ref],
                 forward_sum=ref2forward_sum.get(ref, None),
                 reverse_sum=ref2reverse_sum.get(ref, None),
@@ -276,8 +337,8 @@ class CCResult(object):
                 mappable_forward_sum=mappable_ref2forward_sum.get(ref, None),
                 mappable_reverse_sum=mappable_ref2reverse_sum.get(ref, None),
                 mappable_ccbins=mappable_ref2ccbins.get(ref, None),
-                filter_mask_len=filter_mask_len
-            ) for ref in references
+                filter_mask_len=self.filter_mask_len
+            ) for ref in self.references
         }
 
         #
@@ -285,10 +346,9 @@ class CCResult(object):
             self.skip_ncc = False
             forward_sum = sum(list(ref2forward_sum.values()))
             reverse_sum = sum(list(ref2reverse_sum.values()))
-            ccbins = np.sum(_skip_none(ref2ccbins.values()), axis=0)
         else:
             self.skip_ncc = True
-            forward_sum = reverse_sum = ccbins = None
+            forward_sum = reverse_sum = None
         if forward_sum is not None:
             if forward_sum == 0:
                 logger.error("There is no forward read.")
@@ -303,11 +363,9 @@ class CCResult(object):
             self.calc_masc = True
             mappable_forward_sum = np.sum(_skip_none(mappable_ref2forward_sum.values()), axis=0)
             mappable_reverse_sum = np.sum(_skip_none(mappable_ref2reverse_sum.values()), axis=0)
-            mappable_ccbins = np.sum(_skip_none(mappable_ref2ccbins.values()), axis=0)
-            mappable_len = np.sum(list(ref2mappable_len.values()), axis=0)
         else:
             self.calc_masc = False
-            mappable_forward_sum = mappable_reverse_sum = mappable_ccbins = mappable_len = None
+            mappable_forward_sum = mappable_reverse_sum = None
         if mappable_forward_sum is not None:
             if np.all(mappable_forward_sum == 0):
                 logger.error("There is no forward read.")
@@ -315,18 +373,3 @@ class CCResult(object):
             elif np.all(mappable_reverse_sum == 0):
                 logger.error("There is no reverse read.")
                 raise ReadsTooFew
-
-        #
-        self.whole = PyMaSCStats(
-            read_len, filter_len, expected_library_len,
-            genomelen=sum(lengths),
-            forward_sum=forward_sum,
-            reverse_sum=reverse_sum,
-            ccbins=ccbins,
-            mappable_len=mappable_len,
-            mappable_forward_sum=mappable_forward_sum,
-            mappable_reverse_sum=mappable_reverse_sum,
-            mappable_ccbins=mappable_ccbins,
-            warning=True,
-            filter_mask_len=filter_mask_len
-        )
